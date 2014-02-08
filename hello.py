f887b13df5f85e9b2e15b5100c48ff198b9446f8
Stella Pantela <stella.pantela@gmail.com>
2014-02-08 17:08:50 -0500

    json debugged

--- hello.py
+++ hello.py
@@ -6,24 +6,15 @@ from amazon import run_test
 
 app = Flask(__name__)
 
-results = [
-    {
-      "imgUrl": "http://ecx.images-amazon.com/images/I/410qw8Bt8AL.jpg", 
-      "pageUrl": "http://www.amazon.com/Estes-1469-Tandem-X-Launch-Set/dp/B002VLP67S%3FSubscriptionId%3DAKIAJKTEYNKJYROFSX3Q%26tag%3Dmunerum-20%26linkCode%3Dxm2%26camp%3D2025%26creative%3D165953%26creativeASIN%3DB002VLP67S", 
-      "price": "$19.87", 
-      "title": "Estes 1469 Tandem-X Launch Set"
-    }];
-
 @app.route('/products', methods = ['GET'])
 def get_products():
-	return make_response(jsonify( { 'results': results } ), 200)
-
+	return make_response(jsonify( { 'error': "Specify input" } ), 200)
 
 @app.route('/products/<string:keyword>', methods = ['GET'])
 def get_product(keyword):
-    keyword = filter(lambda t: t['keyword'] == keyword, products)
-    if not keyword:
-        abort(404)
+	if not keyword:
+		abort(404)
+	results = run_test('All', keyword, 'Images, ItemAttributes, OfferSummary')
 	return make_response(jsonify( { 'results': results } ), 200)
 
 
