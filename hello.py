cc47e138bd42eab84769c3f2d87b9aa1a3110aee
Stella Pantela <stella.pantela@gmail.com>
2014-02-09 07:46:33 -0500

    bugfix

--- hello.py
+++ hello.py
@@ -62,7 +62,7 @@ def set_answer(user_id, question_id, answer_text, callback):
 def get_recs(user_id, callback):
 	if (not callback):
 		abort(404)
-	if (user_id == 1387154996):
+	if (user_id == '1387154996'):
 		user_id = 21
 	answers = db.get_answers(user_id)
 	answers = answers['results']
